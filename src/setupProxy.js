9b5e5f741eb45a2cde246d375eeb46f20205aef5
amor <amoriqbalmail@gmail.com>
2020-06-30 13:49:26 +0530

    try proxy bypass

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,7 @@
-const {createProxyMiddleware}=require('http-proxy-middleware')
-
+const {createProxyMiddleware}=require('http-proxy-middleware');
+const { env } = require('process');
+if(process.env.NODE_ENV!=='production'){
 module.exports=function(app){
     app.use(createProxyMiddleware(['/api','/auth/google'],{target:'http://localhost:5000'}));
-};
+}
+}
