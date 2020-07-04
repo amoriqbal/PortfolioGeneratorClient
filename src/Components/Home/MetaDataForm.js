f01a3f4378f0503f4b2b1a0fddae3a275216c5b4
amor <amoriqbalmail@gmail.com>
2020-07-04 10:27:10 +0530

    meta form complete

--- src/Components/Home/MetaDataForm.js
+++ src/Components/Home/MetaDataForm.js
@@ -4,7 +4,11 @@ import moment from 'moment'
 
 export default function(){
     const [userdata,setUserdata]=useState(null)
-    const [achievements,setAchievemets]=useState([])
+    const [achievements,setAchievements]=useState([])
+    const [qualifications,setQualifications]=useState([])
+    const [works,setWorks]=useState([])
+    const [name,setName]=useState('')
+    const [about,setAbout] = useState('')
     useEffect(()=>{       
         const updateUserdata= async()=>{
             var res=await Axios.get('/api/auth/current_user')
@@ -17,7 +21,11 @@ export default function(){
                 var uname=res.data.uname
                 var data=await Axios.get(`/api/fetchuserdata/${uname}`)
                 await setUserdata(data.data)
-                await setAchievemets(data.data.meta.achievements)
+                await setAchievements(data.data.meta.achievements||[])
+                await setQualifications(data.data.meta.qualifications||[])
+                await setWorks(data.data.meta.works||[])
+                await setName(data.data.meta.name||"")
+                await setAbout(data.data.meta.about||"")
             }
         }
         
@@ -26,7 +34,7 @@ export default function(){
     const submitUserdata=async()=>{
         //await setUserdata({...{meta:{...userdata.meta,...{achievements:achievements}}}})
         var sendData=JSON.parse(JSON.stringify(userdata))
-        sendData.meta.achievements=achievements
+        sendData.meta={...{name,about,achievements,works,qualifications},}
         var res=await Axios.post('/api/updateuserdata',{
             meta:sendData.meta
         })
@@ -41,8 +49,215 @@ export default function(){
     }
     return(
         <>
-        <h1>{(userdata&&userdata.meta&&userdata.meta.name?userdata.meta.name:"noname")}</h1>
+            <h3>
+            <label htmlFor='name'>Name</label>
+            <input 
+                name="name"
+                value={name}
+                type='textarea'
+                onChange={async (e)=>{
+                    await setName(e.target.value)
+                }}
+            />
+            </h3>
+            <label htmlFor='about'>About</label>
+            <input 
+                name="about"
+                value={about}
+                type='textarea'
+                onChange={async (e)=>{
+                    await setAbout(e.target.value)
+                }}
+            />
+
+
+        {/*Qualifications*/}
         <div>
+            <h3>Qualifications</h3>
+            <table>
+                <thead>
+                    <tr><th>Name</th><th>Field</th><th>Start Date</th><th>End Date</th><th>Grade</th><th>Institution</th></tr>
+                </thead>
+                <tbody>
+                {
+                    (qualifications && qualifications.map((value,index)=>
+                        (<tr key={index.toString()}>
+                            <td>
+                                <input 
+                                    name={`qualification_name_${index}`} 
+                                    type='textarea'
+                                    value={value.name} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedQualifications=JSON.parse(JSON.stringify(qualifications))
+                                        updatedQualifications[index]={...{name:e.target.value}}
+                                        setQualifications(updatedQualifications)
+                                    }}
+                                />
+                            </td>
+                            <td>
+                                <input 
+                                    name={`qualification_field_${index}`} 
+                                    type='textarea'
+                                    value={value.field} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedQualifications=JSON.parse(JSON.stringify(qualifications))
+                                        updatedQualifications[index]={...{field:e.target.value}}
+                                        setQualifications(updatedQualifications)
+                                    }}
+                                />
+                            </td>
+                            <td>
+                                <input 
+                                    name={`qualification_startDate_${index.toString()}`} 
+                                    type='date'
+                                    value={moment(value.startDate).format('YYYY-MM-DD')} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedQualifications=JSON.parse(JSON.stringify(qualifications))
+                                        updatedQualifications[index]={...{startDate:e.target.value}}
+                                        setQualifications(updatedQualifications)
+                                    }}
+                                />
+                            </td>
+                            <td>
+                                <input 
+                                    name={`qualification_endDate_${index.toString()}`} 
+                                    type='date' 
+                                    value={moment(value.endDate).format('YYYY-MM-DD')} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedQualifications=JSON.parse(JSON.stringify(qualifications))
+                                        updatedQualifications[index]={...{endDate:e.target.value}}
+                                        setQualifications(updatedQualifications)
+                                    }}
+                                />
+                            </td>
+                            <td>
+                                <input 
+                                    name={`qualification_grade_${index}`} 
+                                    type='textarea'
+                                    value={value.grade} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedQualifications=JSON.parse(JSON.stringify(qualifications))
+                                        updatedQualifications[index]={...{grade:e.target.value}}
+                                        setQualifications(updatedQualifications)
+                                    }}
+                                />
+                            </td>
+                            <td>
+                                <input 
+                                    name={`qualification_institution_${index}`} 
+                                    type='textarea'
+                                    value={value.institution} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedQualifications=JSON.parse(JSON.stringify(qualifications))
+                                        updatedQualifications[index]={...{institution:e.target.value}}
+                                        setQualifications(updatedQualifications)
+                                    }}
+                                />
+                            </td>
+                        </tr>)
+                    ))
+                }
+                </tbody>
+            </table>
+            <button 
+                onClick={(e)=>{
+                    e.preventDefault()
+                    setQualifications(qualifications.concat([{date:new Date(10,10,10),description:"New Qualification"}]))
+                }}>
+                Add Qualification
+            </button>
+        </div>
+        {/*Qualifications*/}
+        
+
+        {/*Works*/}
+        <div>
+            <h3>Work Experience</h3>
+            <table>
+                <thead>
+                    <tr><th>Description</th><th>Start Date</th><th>End Date</th><th>Institution</th></tr>
+                </thead>
+                <tbody>
+                {
+                    (works && works.map((value,index)=>
+                        (<tr key={index.toString()}>
+                            <td>
+                                <input 
+                                    name={`work_description_${index}`} 
+                                    type='textarea'
+                                    value={value.description} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedWorks=JSON.parse(JSON.stringify(works))
+                                        updatedWorks[index]={...{description:e.target.value}}
+                                        setWorks(updatedWorks)
+                                    }}
+                                />
+                            </td>
+                            <td>
+                                <input 
+                                    name={`work_startDate_${index.toString()}`} 
+                                    type='date' 
+                                    value={moment(value.startDate).format('YYYY-MM-DD')} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedWorks=JSON.parse(JSON.stringify(works))
+                                        updatedWorks[index]={...{startDate:e.target.value}}
+                                        setWorks(updatedWorks)
+                                    }}
+                                />
+                            </td>
+
+                            <td>
+                                <input 
+                                    name={`work_endDate_${index.toString()}`} 
+                                    type='date' 
+                                    value={moment(value.endDate).format('YYYY-MM-DD')} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedWorks=JSON.parse(JSON.stringify(works))
+                                        updatedWorks[index]={...{endDate:e.target.value}}
+                                        setWorks(updatedWorks)
+                                    }}
+                                />
+                            </td>
+
+                            <td>
+                                <input 
+                                    name={`work_institution_${index}`} 
+                                    type='textarea'
+                                    value={value.institution} 
+                                    onChange={(e)=>{
+                                        e.preventDefault()
+                                        var updatedWorks=JSON.parse(JSON.stringify(works))
+                                        updatedWorks[index]={...{institution:e.target.value}}
+                                        setWorks(updatedWorks)
+                                    }}
+                                />
+                            </td>
+                        </tr>)
+                    ))
+                }
+                </tbody>
+            </table>
+            <button 
+                onClick={(e)=>{
+                    e.preventDefault()
+                    setWorks(works.concat([{startDate:new Date(10,10,10),endDate:new Date(10,10,10),description:"New Work"}]))
+                }}>
+                Add Work
+            </button>
+        </div>
+        
+        {/*Achievements*/}
+        <div>
+            <h3>achievements</h3>
             <table>
                 <thead>
                     <tr><th>Description</th><th>Date</th></tr>
@@ -58,9 +273,9 @@ export default function(){
                                     value={value.description} 
                                     onChange={(e)=>{
                                         e.preventDefault()
-                                        var updatedAchievemets=JSON.parse(JSON.stringify(achievements))
-                                        updatedAchievemets[index]={...{description:e.target.value}}
-                                        setAchievemets(updatedAchievemets)
+                                        var updatedAchievements=JSON.parse(JSON.stringify(achievements))
+                                        updatedAchievements[index]={...{description:e.target.value}}
+                                        setAchievements(updatedAchievements)
                                     }}
                                 />
                             </td>
@@ -71,9 +286,9 @@ export default function(){
                                     value={moment(value.date).format('YYYY-MM-DD')} 
                                     onChange={(e)=>{
                                         e.preventDefault()
-                                        var updatedAchievemets=JSON.parse(JSON.stringify(achievements))
-                                        updatedAchievemets[index]={...{date:e.target.value}}
-                                        setAchievemets(updatedAchievemets)
+                                        var updatedAchievements=JSON.parse(JSON.stringify(achievements))
+                                        updatedAchievements[index]={...{date:e.target.value}}
+                                        setAchievements(updatedAchievements)
                                     }}
                                 />
                             </td>
@@ -85,14 +300,17 @@ export default function(){
             <button 
                 onClick={(e)=>{
                     e.preventDefault()
-                    setAchievemets(achievements.concat([{date:new Date(10,10,10),description:"New Achievement"}]))
+                    setAchievements(achievements.concat([{date:new Date(10,10,10),description:"New Achievement"}]))
                 }}>
                 Add Achievement
             </button>
         </div>
+        {/*Achievements*/}
+
         <button 
             onClick={(e)=>{
                 submitUserdata()
+
             }}>
             Submit
         </button>
--- src/Templates/TemplateBase.js
+++ src/Templates/TemplateBase.js
@@ -7,8 +7,48 @@ const TemplateBase=(props)=>{
     }
     return(
         <>
-        <h1>{(userdata&&userdata.meta&&userdata.meta.name?userdata.meta.name:"noname")}</h1>
+        <h1>{(userdata&&userdata.meta&&userdata.meta.name?userdata.meta.name:"MyName")}</h1>
+        <p>{(userdata&&userdata.meta&&userdata.meta.about?userdata.meta.about:"")}</p>
         <div>
+            <h3>Qualifications</h3>
+            <table>
+                <thead>
+                    <tr><th>Name</th><th>Field</th><th>Start Date</th><th>End Date</th><th>Grade</th><th>Institution</th></tr>
+                </thead>
+                <tbody>
+                {
+                    (userdata && userdata.meta && userdata.meta.qualifications.map((value,index)=>
+                        (<tr key={index.toString()}>
+                            <td>{value.name}</td>
+                            <td>{value.field}</td>
+                            <td>{value.startDate.toString()}</td>
+                            <td>{value.endDate.toString()}</td>
+                            <td>{value.grade}</td>
+                            <td>{value.institution}</td>
+                        </tr>)))
+                }
+                </tbody>
+            </table>
+
+            <h3>Work Experience</h3>
+            <table>
+                <thead>
+                    <tr><th>Description</th><th>Start Date</th><th>End Date</th><th>Institution</th></tr>
+                </thead>
+                <tbody>
+                {
+                    (userdata && userdata.meta && userdata.meta.works.map((value,index)=>
+                        (<tr key={index.toString()}>
+                            <td>{value.description}</td>
+                            <td>{value.startDate.toString()}</td>
+                            <td>{value.endDate.toString()}</td>
+                            <td>{value.institution}</td>
+                        </tr>)))
+                }
+                </tbody>
+            </table>
+
+            <h3>Achievements</h3>
             <table>
                 <thead>
                     <tr><th>Description</th><th>Date</th></tr>
